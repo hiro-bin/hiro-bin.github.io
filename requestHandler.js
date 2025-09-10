4e597270fdd956abef9218381098d2f5b252e453
KimHyeonSu <hirobin2001@gmail.com>
2025-09-10 12:18:26 +0900

    First Commit

---
+++ requestHandler.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const main_view = fs.readFileSync('./main.html', 'utf-8');
+const calculator_view = fs.readFileSync('./calculator.html', 'utf-8');
+const lotto_view = fs.readFileSync('./lotto.html', 'utf-8');
+const todolist_view = fs.readFileSync('./todolist.html', 'utf-8');
+const css_view = fs.readFileSync('./style.css', 'utf-8');
+
+function main(response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(main_view);
+    response.end();
+}
+
+function calculator(response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(calculator_view);
+    response.end();
+}
+
+function lotto(response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(lotto_view);
+    response.end();
+}
+
+function todolist(response) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(todolist_view);
+    response.end();
+}
+
+function css(response) {
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    response.write(css_view);
+    response.end();
+}
+
+let handle = {};
+handle['/'] = main;
+handle['/calculator'] = calculator;
+handle['/lotto'] = lotto;
+handle['/todolist'] = todolist;
+handle['/style.css'] = css;
+
+exports.handle = handle;
---
+++ router.js
@@ -0,0 +1,11 @@
+function route(pathname, handle, response, productId) {
+    if(typeof(handle[pathname]) === 'function') {
+        handle[pathname](response, productId);
+    } else {
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        response.write('Not Found');
+        response.end();
+    }
+}
+
+exports.route = route;
